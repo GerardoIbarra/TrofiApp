4d4acd771c0296a82f87b6f2c9256489ce976aa6
Luis Ibarra <luissustayd8@gmail.com>
2026-03-29 17:58:09 -0600

    chore: hardcode EAS project ID and update URL in app.json and remove dynamic configuration from app.config.js

--- app.config.js
+++ app.config.js
@@ -1,16 +1,5 @@
 module.exports = ({ config }) => {
   return {
     ...config,
-    extra: {
-      ...config.extra,
-      router: {},
-      eas: {
-        projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
-      },
-    },
-    updates: {
-      ...config.updates,
-      url: process.env.EXPO_PUBLIC_PROJECT_URL,
-    },
   };
 };
