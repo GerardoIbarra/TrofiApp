91195e6964fcb355f8551f0917013ad8d40414b2
Luis Ibarra <luissustayd8@gmail.com>
2026-09-01 00:02:01 -0600

    chore: migrate Hermes transform profile from metro.config.js to babel.config.js

--- metro.config.js
+++ metro.config.js
@@ -6,7 +6,4 @@ const config = getDefaultConfig(__dirname);
 config.resolver.unstable_enablePackageExports = true;
 config.resolver.sourceExts.push('mjs');
 
-// Fix Hermes bytecode compilation for private class fields (#x, #y etc.)
-config.transformer.unstable_transformProfile = 'hermes-stable';
-
 module.exports = config;
